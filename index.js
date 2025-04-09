3f329e0bacabadb72c0139a73f7f33297de1d231
vipul7700 <149236070+vipul7700@users.noreply.github.com>
2025-04-09 18:47:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware setup
 //const cors = require('cors');
-app.use(cors({
-    origin: 'http://localhost:3000', // Adjust the origin if necessary
-}));
+//app.use(cors({
+  //  origin: 'http://localhost:3000', // Adjust the origin if necessary
+//}));
+
 
 //app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://vercel-frontend-ruddy.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
